32daeec06e16b97876e7b93997f856b8cc832d00
Ellie Lambert <ebezjian@gmail.com>
2021-01-22 01:33:29 -0500

    add client directory and new packages

---
+++ CretaceousClient.Solution/CretaceousClient/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace cretaceousclient.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
--- CretaceousClient.Solution/CretaceousClient/Program.cs
+++ CretaceousClient.Solution/CretaceousClient/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace cretaceouspark
+namespace cretaceousclient
 {
     public class Program
     {
@@ -20,6 +20,5 @@ namespace cretaceouspark
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>();
-                
     }
 }
